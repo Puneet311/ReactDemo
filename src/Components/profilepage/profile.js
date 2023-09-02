0039a91200d5e97efdbc173babfa6171c8db7c7e
Puneet311 <puneetpandey311@gmail.com>
2023-09-02 18:12:13 +0530

    button fix

--- src/Components/profilepage/profile.js
+++ src/Components/profilepage/profile.js
@@ -1,14 +1,35 @@
 import  { Component } from 'react'
+import { Navigate } from "react-router-dom"
 import './profile.scss'
+import file from "../../assets/cv.pdf";
 class Profile extends Component {
+constructor(){
+    super()
+
+    this.state={navigate:false}
+}
+
+handleHireMe= ()=>{
+    this.setState({
+        navigate:true
+    })
+}
+
+downLoadCv= ()=>{
+    console.log('download')
+    window.open(file)
+}
+
     render() {
         return (
             <div>
-                <h1 className="nameHeading">Hi, I am <span className="name">Puneet</span></h1>
-                <p className="nameSubHeading">I'm a web developer based out in Delhi.</p>
-                <div>
-                <button className="basicButton hireButton">Hire me</button>
-                <button className="basicButton downloadButton">Download CV</button>
+                <h1 className="heading-profile nameHeading">Hi, I am <span className="name">Puneet</span></h1>
+                <p className="heading-profile nameSubHeading">I'm a software developer based out in Delhi.</p>
+                <div className="buttons">
+                <button className="basicButton hireButton" onClick={()=>this.handleHireMe()}>Hire me</button>
+                {/* <a href={file} download = "cv.pdf" className="basicButton downloadButton">Download CV</a> */}
+                <button className="basicButton downloadButton" onClick={()=>this.downLoadCv()}>Download CV</button>
+                {this.state.navigate && <Navigate to='/ReactDemo/contact'/>}
                 </div>
             </div>
         )
--- src/Components/projects/projects.js
+++ src/Components/projects/projects.js
@@ -48,36 +48,11 @@ function Projects() {
       },
       {
         pic: pic,
-        heading: "SIM WEB",
-        description:
-          "SIM is stock inventory move application of belk, it is used to track the inventory travel status. I was the part of this project to build the UI using Angular, Angular Material,Html, Css. My main task was to build the features from scratch, fix the bugs and to get and send data to server using Http request.",
+        heading: "Markdown Reports",
+        description:"Markdown report is a part of sim-web, this is the dedicated web-app where user can view and download different kinds of report invovled in inventory movement.",
         flag: false,
         id:5
-      },
-      {
-          pic: pic2,
-          heading: "Skill Management",
-          description:
-            "I worked in this project for 2 years in infosys. I was responsible for building ui features from scratch using Angular. Angular Material, HTML, CSS.",
-          flag: false,
-          id:6
-        },
-        {
-          pic: pic3,
-          heading: "Git Repo Finder",
-          description:
-            "I build this as my personal project while learning react in this user can find the repo of anyone on github. I used React, SASS, Html and github's public api.",
-          flag: true,
-          id:7
-        },
-        {
-          pic: pic4,
-          heading: "Weather App",
-          description:
-            "I created this project while i was learning Angular. I used Open Weather API ,Angular, CSS,HTML for creating this application.",
-          flag: true,
-          id:8
-        },
+      }
   ];
   const swiperElRef = useRef(null);
   return (
